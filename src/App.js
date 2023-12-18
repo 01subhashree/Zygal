0bed27ae4863ab904f8a656833b8835213dd32a6
01subhashree/ <ssubhashree094@gmail.com>
2023-12-18 16:54:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [boxCount, setBoxCount] = useState(0);
+  const [boxSize, setBoxSize] = useState(null);
+  const [boxes, setBoxes] = useState([]);
+
+  const handleGenerateBoxes = () => {
+    if (boxCount % 2 !== 0 || boxCount <= 0) {
+      alert("Please enter a valid even number greater than 0.");
+      return;
+    }
+
+    const newBoxes = Array.from({ length: boxCount }, (_, index) => index + 1);
+    setBoxes(newBoxes);
+
+    setBoxSize(100 / boxCount);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-container">
+        <input
+          type="number"
+          value={boxCount}
+          onChange={(e) => setBoxCount(parseInt(e.target.value, 10))}
+        />
+        <button onClick={handleGenerateBoxes}>Generate</button>
+      </div>
+      <div className="box-container">
+        {boxes.map((index) => (
+          <div
+            key={index}
+            className="black-box"
+            style={{
+              width: `${boxSize}%`,
+              height: `${boxSize}%`,
+              transform: `translate(${(boxCount - index) * 100}%, ${
+                (boxCount - index) * 100
+              }%)`,
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
